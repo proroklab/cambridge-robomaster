c1adaee53acbbc554e03e2951acd7f25e59913a9
J. Blumenkamp <jb2270@cam.ac.uk>
2024-02-07 15:38:41 +0000

    nmcli setup

--- setup/nmcli.bash
+++ setup/nmcli.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
-host_id=$($(hostname) | sed 's/robomaster-//')
+host_id=$(echo $(hostname) | sed 's/robomaster-//')
 ip_last_octet=$(($host_id + 2))
 
 nmcli con add con-name robomaster type wifi ifname wlan0 ssid "robomaster" -- wifi-sec.key-mgmt wpa-psk wifi-sec.psk "**r0b0t**" ipv4.method manual ipv4.addresses 10.3.1.$ip_last_octet/24 ipv4.gateway 10.3.1.1 ipv4.dns 10.3.1.1 ipv4.routes "192.168.0.0/24 10.3.1.1"
