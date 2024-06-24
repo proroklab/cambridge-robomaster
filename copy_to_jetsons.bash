717fac756165d4778e724163ff7489c31eb9fc1e
J. Blumenkamp <jb2270@cam.ac.uk>
2024-06-24 14:26:14 +0100

    Multi-jetson copy

--- copy_to_jetsons.bash
+++ copy_to_jetsons.bash
@@ -1,4 +1,6 @@
-for ip in 10.3.1.2 10.3.1.3 10.3.1.4; do
-  rsync -a --delete . nvidia@$ip:/home/nvidia/Robot
+declare -a arr=(2 3)
+for i in "${arr[@]}"
+do
+  rsync -a --progress --exclude paper --exclude docs . nvidia@10.3.1.$i:/home/nvidia/Robot
 done
 
