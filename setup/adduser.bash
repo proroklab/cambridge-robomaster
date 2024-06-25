060486fb732487d1040dade631a24f7c149db490
J. Blumenkamp <jb2270@cam.ac.uk>
2024-06-25 23:49:30 +0100

    Fix broken adduser script

--- setup/adduser.bash
+++ setup/adduser.bash
@@ -1,3 +1,11 @@
+#!/bin/bash
+
+# Check if the script is run as root
+if [ "$EUID" -ne 0 ]; then
+    echo "Please run as root or with sudo"
+    exit 1
+fi
+
 read -p "Enter a user name: " username
 
 # check length
@@ -7,9 +15,8 @@ if [ ${#username} -lt 4 ]; then
 fi
 
 read -p "Add user ${username}? (y/n)" confirm
-if [[ $confirm == [yY] ]]; then
+if [[ $confirm =~ [yY] ]]; then
     adduser ${username}
-    usermod -aG sudo docker ${username}
+    usermod -aG sudo,docker ${username}
     echo "${username} ALL=(ALL:ALL) NOPASSWD: ALL" | tee /etc/sudoers.d/${username}
 fi
-
